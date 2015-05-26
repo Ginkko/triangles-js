c92d0b016bc30fdf526f613f826289d542e7c047
Alec Arme & alex caste <student@epicodus.com>
2015-05-26 10:56:16 -0700

    add scalene check. passes spec.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ var triangles = function(a,b,c) {
   triangle.push(a, b, c)
   triangle.sort
 
-debugger;
-
   if ( triangle[0] === triangle[1] && triangle[1] === triangle[2]) {
     return "equilateral";
+  } else if ( triangle[0] === triangle[1] && triangle[2] !== triangle[1]) {
+    return "scalene";
   } else {
   return false;
   }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe ('triangles', function() {
   it("will return false if it is not a valid triangles", function() {
-    expect(triangles(1,1,8)).to.equal(false);
+    expect(triangles(1,2,8)).to.equal(false);
   });
 
   it("will return equilateral if the triangle is equilateral", function(){

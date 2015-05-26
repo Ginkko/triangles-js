4092e3c238c3c5c0bb12ebf39144f8aff10149c5
Alec Arme & alex caste <student@epicodus.com>
2015-05-26 10:48:44 -0700

    add passing spec for equilateral triangles

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 var triangles = function(a,b,c) {
+  if ( a === b && b === c) {
+    return "equilateral";
+  } else {
   return false;
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe ('triangles', function() {
   it("will return false if it is not a valid triangles", function() {
     expect(triangles(1,1,8)).to.equal(false);
   });
+
+  it("will return equilateral if the triangle is equilateral", function(){
+    expect(triangles(3,3,3)).to.equal("equilateral");
+  });
 });
